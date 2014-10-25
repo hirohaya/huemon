48a9c2a12efb4c0445af591266780756f4796378
fichberg <fichberg@fichberg.(none)>
2014-10-24 23:26:18 -0200

    Improved pokemon creation and added a pokedex to relate a pokemon name to its types.

--- pokemon.py
+++ pokemon.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import moves
+from pokedex import pokedex, pokemon_choice
 from myfunctions import print_char
 
 class Pokemon(object):
@@ -10,10 +11,25 @@ class Pokemon(object):
         print_char("Let's start by creating your POKeMON!\n")
         sure = "No"
         while(sure == "No"):
-            #Set your POKeMON's name
-            print_char("\nPlease, enter with the desired POKeMON:\n")
-            name = str(raw_input())
-
+            #Set your POKeMON's name and its respective types
+            name = None
+            types = None
+            while name == None:
+                print_char("\nPlease, enter with the desired POKeMON:\n")
+                name = str(raw_input())
+                get_dictionary = pokemon_choice(name.capitalize())
+                if  get_dictionary == None:
+                    name = None
+                    print_char("\nInvalid name. Type a name of a pokemon from the first generation.\n")
+                else:
+                    for k, v in get_dictionary.items():
+                        name = k
+                        types = v
+                        break
+            split_string = types.split(' ')
+            type1 = split_string[0]
+            type2 = split_string[1]
+            print_char("\nSo you have choosen a "+name+". Its types are "+type1+"/"+type2+". A nice choice!\n")
             #Set your POKeMON's level
             level = -1
             while level == -1:
@@ -22,35 +38,28 @@ class Pokemon(object):
                     level = int(raw_input())
                     if level < 0 or level > 99:
                         level = -1
-                        print "\nInvalid number. The level must be a number from 1 to 99. Let's retry this.\n"
+                        print_char("\nInvalid number. The level must be a number from 1 to 99. Let's retry this.\n")
                 except ValueError:
-                    print "\nNot a number. Let's retry this.\n"
-            
-            #Set your POKeMON's type(s)
-            print_char("\nWhat is its first type?\n")
-            type1 = str(raw_input())
-            print_char("\nIf it does have a second type, which one is it?"\
-                       " If it has only one type, type 'Blank'.\n(Sorry for the pun)\n")
-            type2 = str(raw_input())
-            
+                    print_char("\nNot a number. Let's retry this.\n")
+
             #Set your POKeMON status, please use the official status
             print_char("\nNow let's set your POKeMON's status!\n")
             
             #Set HP
             print_char("\nHow much HP?\n")
-            hp = int(input())
+            hp = int(raw_input())
             #Set Attack
             print_char("\nHow much Attack?\n")
-            attack = int(input())
+            attack = int(raw_input())
             #Set Defense
             print_char("\nHow much Defense?\n")
-            defense = int(input())
+            defense = int(raw_input())
             #Set Special
             print_char("\nHow much Special?\n")
-            special = int(input())
+            special = int(raw_input())
             #Set Speed
             print_char("\nHow much Speed?\n")
-            speed = int(input())
+            speed = int(raw_input())
             
             #Display the newborn POKeMON's info
             print_char("\nYour POKeMON status are:\n\n")
@@ -61,25 +70,35 @@ class Pokemon(object):
             print("\tHP: %d\n\tAttack: %d\n\tDefense: %d\n\tSpecial: %d\n\tSpeed: %d" %(hp, attack, defense, special, speed))
             print()
             print_char("Do you want to continue?\n(Yes or No)\n")
-            sure = input()
-            
-            if(sure == "No"):
-                print_char("\nOk, let's do it again...\n\n")
-            #I'm assuming you are a good user and don't type anything else than Yes or No
-            else:
-                self.name = name
-                self.level = level
-                self.type1 = type1
-                self.type2 = type2
-                
-                #Set status on constructor
-                self.hp = hp
-                self.attack = attack
-                self.defense = defense
-                self.special = special
-                self.speed = speed
-                print_char("\nOk! Your POKeMON has been created! Let's continue!\n\n")
-        
+            #Check if user is sure about his options
+            decision = "No"
+            while decision.capitalize() == "No":
+                decision = raw_input() 
+                #User is not sure
+                if(decision.capitalize() == "No"):
+                    print_char("\nOk, let's do it again...\n\n")
+                    break
+                #The user typed he is sure
+                elif(decision.capitalize() == "Yes"):
+                    self.name = name
+                    self.level = level
+                    self.type1 = type1
+                    self.type2 = type2
+                    
+                    #Set status on constructor
+                    self.hp = hp
+                    self.attack = attack
+                    self.defense = defense
+                    self.special = special
+                    self.speed = speed
+                    print_char("\nOk! Your POKeMON has been created! Let's continue!\n\n")
+                    sure = "Yes"
+                    decision = "Yes"
+                #Troll user. You have to answer Yes or no!
+                else:
+                    print_char("\nWell... I was expecting for a Yes or a No, so please, don't make the things hard and answer my question, I don't have all day.\n")
+                    decision = "No"
+
     ##Set the pokemon's status
     #def status(self, hp, attack, defense, special, speed):
     #    self.hp = hp
