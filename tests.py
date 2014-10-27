7517aba000ba025f50cf6e19497dd2421646e5c5
Leonardo Schäffer <leonardo.schaffer@gmail.com>
2014-10-26 22:58:38 -0200

    Tests

--- tests.py
+++ tests.py
@@ -14,30 +14,30 @@ class TestPokemonTextGame(unittest.TestCase):
 		self.damage = 40
 		self.hp = self.pokemon1.hp
 
-	#def test_subtract_damage(self):
-	#	expected_hp = self.pokemon1.hp - self.damage
-	#	if expected_hp < 0:
-	#		expected_hp = 0
-	#	self.pokemon1.subtract_damage(self.damage)
-	#	self.assertEqual(self.pokemon1.hp, expected_hp)
+	def test_subtract_damage(self):
+		expected_hp = self.pokemon1.hp - self.damage
+		if expected_hp < 0:
+			expected_hp = 0
+		self.pokemon1.subtract_damage(self.damage)
+		self.assertEqual(self.pokemon1.hp, expected_hp)
 
-	#def test_is_defeated(self):
-	#	#Derrotado
-	#	self.pokemon1.hp = 0
-	#	self.assertEqual(self.pokemon1.is_defeated(), True)
-	#	#Nao Derrotado
-	#	self.pokemon1.hp = self.hp
-	#	self.assertEqual(self.pokemon1.is_defeated(), False)
+	def test_is_defeated(self):
+		#Defeated
+		self.pokemon1.hp = 0
+		self.assertEqual(self.pokemon1.is_defeated(), True)
+		#Not defeated
+		self.pokemon1.hp = self.hp
+		self.assertEqual(self.pokemon1.is_defeated(), False)
 
-	#def test_calculate_damage(self):
-	#	expected_minimum_modifier = 0
-	#	expected_maximum_modifier = 1.5 * 4 * 2 * 1 
-	#
-	#	expected_minimum_damage = int(((((2 * self.pokemon1.level) + 10) / 250) * (self.pokemon1.attack / self.pokemon2.defense) * self.pokemon1.move1.move_power + 2) * expected_minimum_modifier)
-	#	expected_maximum_damage = int(((((2 * self.pokemon1.level) + 10) / 250) * (self.pokemon1.attack / self.pokemon2.defense) * self.pokemon1.move1.move_power + 2) * expected_maximum_modifier)
-	#	#The damage must be inside a given interval, delimited by the maximum and minimum values of the modifier (as the rest of the formula is static to the respectives pokemons involved)
-	#	self.assertGreaterEqual(self.pokemon1.calculate_damage(self.pokemon1.move1, self.pokemon2), expected_minimum_damage)
-	#	self.assertLessEqual(self.pokemon1.calculate_damage(self.pokemon1.move1, self.pokemon2), expected_maximum_damage)
+	def test_calculate_damage(self):
+		expected_minimum_modifier = 0
+		expected_maximum_modifier = 1.5 * 4 * 2 * 1 
+	
+		expected_minimum_damage = int(((((2 * self.pokemon1.level) + 10) / 250) * (self.pokemon1.attack / self.pokemon2.defense) * self.pokemon1.move1.move_power + 2) * expected_minimum_modifier)
+		expected_maximum_damage = int(((((2 * self.pokemon1.level) + 10) / 250) * (self.pokemon1.attack / self.pokemon2.defense) * self.pokemon1.move1.move_power + 2) * expected_maximum_modifier)
+		#The damage must be inside a given interval, delimited by the maximum and minimum values of the modifier (as the rest of the formula is static to the respectives pokemons involved)
+		self.assertGreaterEqual(self.pokemon1.calculate_damage(self.pokemon1.move1, self.pokemon2), expected_minimum_damage)
+		self.assertLessEqual(self.pokemon1.calculate_damage(self.pokemon1.move1, self.pokemon2), expected_maximum_damage)
 
 
 if __name__ == '__main__':
